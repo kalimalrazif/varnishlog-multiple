35ca92619107f7e6cede0f53487837edc7180d34
Nomar Mora <nomarmora@gmail.com>
2016-01-29 08:06:29 -0430

    Perl tidy, mejorada la forma de abrir el archivo de logs ahora no se va quejar porque no esta el archivo a las primeras de cambio, mejorada la lectura inicial, va a leer todo el archivo desde el principio.
    Signed-off-by: Nomar Mora <nomarmora@gmail.com>

--- varnishlog_multiple.pl
+++ varnishlog_multiple.pl
@@ -8,7 +8,6 @@ use IO::File;
 use File::Tail;
 use IO::Handle;
 
-
 $SIG{INT}  = \&cerrar_todo;
 $SIG{TERM} = \&cerrar_todo;
 
@@ -19,25 +18,21 @@ my $varnish_log_feed;
 my $error_log;
 my $mi_log;
 
-
-
-my $log_dir = '/var/log/varnish';
+my $log_dir          = '/var/log/varnish';
 my $apache_sites_dir = '/etc/apache2/sites-enabled';
 my $varnish_log_file = '/var/log/varnish/varnishncsa.log';
-my $error_log_file = '/var/log/varnish/error.log';
-my $mi_log_file = '/var/log/varnishlog_multiple.log';
+my $error_log_file   = '/var/log/varnish/error.log';
+my $mi_log_file      = '/var/log/varnishlog_multiple.log';
 
 $mi_log = new IO::File $mi_log_file, ">>";
 $mi_log->autoflush(1);
 
 print $mi_log "Escribiendo pid file\n";
 
-$pidfile = File::Pid->new({
-   file => '/var/run/varnishlog_multiple.pid'
-});
+$pidfile = File::Pid->new( { file => '/var/run/varnishlog_multiple.pid' } );
 
 if ( my $num = $pidfile->running ) {
-   die "Ya esta corriendo: $num\n";
+    die "Ya esta corriendo: $num\n";
 }
 
 $pidfile->write;
@@ -52,72 +47,83 @@ open_log_files();
 
 print $mi_log "Esperando para abrir el log de varnishncsa\n";
 
-while (1) {
-	last if (-e $varnish_log_file);
-}
+# ya no es necesario pero lo dejo acá por las dudas.
+# while (1) {
+# 	last if (-e $varnish_log_file);
+# }
 
 print $mi_log "Abriendo el log de varnishncsa\n";
 
-$varnish_log_feed=File::Tail->new($varnish_log_file );
+$varnish_log_feed = tie *FH, "File::Tail",
+    (
+    name               => $varnish_log_file,
+    ignore_nonexistant => 1,
+    maxinterval        => 30,
+    tail               => -1,
+    );
 
 print $mi_log "Empezando a procesar el log\n";
 
-while(defined(my $linea = $varnish_log_feed->read)) {
-	my ($host, @resto) = split / /, $linea;
-	$host =~ s/www\.//;
-	$host = lc $host;
-	if (exists $log_files->{$host}) {
-		my $para_imprimir = $log_files->{$host};
-		print $para_imprimir "@resto";	
-	} else {
-		print $error_log "@resto";
-	}
+while (<FH>) {
+    my ( $host, @resto ) = split / /, $_;
+    $host =~ s/www\.//;
+    $host = lc $host;
+    if ( exists $log_files->{$host} ) {
+        my $para_imprimir = $log_files->{$host};
+        print $para_imprimir "@resto";
+    }
+    else {
+        print $error_log "@resto";
+    }
 }
 
 cerrar_todo();
 
 ##
-# Subrutinas 
+# Subrutinas
 ##
 
 sub get_dominios {
-	my $dir = shift;
-	my @tmpdir;
-	opendir(DIR, $dir) or die $!;
-	   while (my $file = readdir(DIR)) {
-	      # Use a regular expression to ignore files beginning with a period
-	      next if ($file =~ m/^\./);
-	      next if ($file =~ m/^0/);
-	      $file =~ s/(.*)\.conf$/$1/;
-		push @tmpdir, $file;
-	}
-	closedir(DIR);
-	return @tmpdir;
+    my $dir = shift;
+    my @tmpdir;
+    opendir( DIR, $dir ) or die $!;
+    while ( my $file = readdir(DIR) ) {
+
+        # Use a regular expression to ignore files beginning with a period
+        next if ( $file =~ m/^\./ );
+        next if ( $file =~ m/^0/ );
+        $file =~ s/(.*)\.conf$/$1/;
+        push @tmpdir, $file;
+    }
+    closedir(DIR);
+    return @tmpdir;
 }
 
-# TODO: Hacer esta rutina mas elegante :-/ 
+# TODO: Hacer esta rutina mas elegante :-/
 
 sub open_log_files {
-	foreach my $dominio (@dominios) {
-		my $archivo = $log_dir . "/access_" . $dominio . ".log";
-		$log_files->{$dominio} = new IO::File $archivo, ">>";
-		die "$archivo no pudo ser abierto" if (!defined $log_files->{$dominio});
-		#my $tmp = IO::File->new($archivo,O_WRONLY|O_APPEND);
-		#$log_files->{$dominio} = $tmp;
- 		$log_files->{$dominio}->autoflush(1);
-	}
-	$error_log = new IO::File $error_log_file, ">>";
-	$error_log->autoflush(1);
-	print $mi_log "Abiertos los archivos!\n";
+    foreach my $dominio (@dominios) {
+        my $archivo = $log_dir . "/access_" . $dominio . ".log";
+        $log_files->{$dominio} = new IO::File $archivo, ">>";
+        die "$archivo no pudo ser abierto"
+            if ( !defined $log_files->{$dominio} );
+
+        #my $tmp = IO::File->new($archivo,O_WRONLY|O_APPEND);
+        #$log_files->{$dominio} = $tmp;
+        $log_files->{$dominio}->autoflush(1);
+    }
+    $error_log = new IO::File $error_log_file, ">>";
+    $error_log->autoflush(1);
+    print $mi_log "Abiertos los archivos!\n";
 }
 
 sub cerrar_todo {
-	foreach my $dominio (keys(%{$log_files})) {
-		undef $log_files->{$dominio};
-	}
-	print $mi_log "Recibida la señal de morir!\n";
-	undef $error_log;
-	undef $mi_log;
-	$pidfile->remove;
-	exit(0);
+    foreach my $dominio ( keys( %{$log_files} ) ) {
+        undef $log_files->{$dominio};
+    }
+    print $mi_log "Recibida la señal de morir!\n";
+    undef $error_log;
+    undef $mi_log;
+    $pidfile->remove;
+    exit(0);
 }
